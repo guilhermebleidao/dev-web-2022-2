95dfd212380ebdf0596d4d0cf85a348818728ca4
Guilherme Bleidão <guilhermenbleidao@gmail.com>
2022-09-15 22:30:42 -0300

    movimentacao pecas dama

--- damas/damas.js
+++ damas/damas.js
@@ -15,16 +15,25 @@ function criaTabuleiro() {
         tabela.append(linha);
         for (let j = 0; j < tamanho; j++) {
             let celula = document.createElement('td');
+            celula.addEventListener('drop', drop)
             linha.append(celula);
 
             celula.style.width = `${tamanhoCelula}px`;
             celula.style.height = `${tamanhoCelula}px`;
             if (i % 2 == j % 2) {
+                celula.addEventListener('dragover', allowDrop)
                 celula.style.backgroundColor = 'black';
                 if (i * 8 + j <= 24) {
-                    celula.append(criaPeca('black'));
+                    const peca = criaPeca('black')
+                    peca.id = `b-i${i}-j${j}`
+                    celula.append(peca)
+                    celula.removeEventListener('dragover', allowDrop)
                 } else if (i * 8 + j >= 40) {
-                    celula.append(criaPeca('red'));
+                    const peca = criaPeca('red')
+                    peca.id = `r-i${i}-j${j}`
+                    peca.draggable = true
+                    celula.append(peca)
+                    celula.removeEventListener('dragover', allowDrop)
                 }
             } else {
                 celula.style.backgroundColor = 'white';
@@ -34,10 +43,37 @@ function criaTabuleiro() {
     return tabela;
 }
 
+function jogadorDaVez() {
+    const pecas = document.querySelectorAll('.peca')
+    pecas.forEach(peca => {
+        peca.draggable = !peca.draggable
+    });
+}
+
 function criaPeca(cor) {
     let imagem = document.createElement('img');
+    imagem.classList.add('peca')
     imagem.setAttribute('src', `${cor}.png`);
     imagem.setAttribute('width', `${tamanhoCelula-4}px`);
     imagem.setAttribute('height', `${tamanhoCelula-4}px`);
+    imagem.setAttribute('draggable', 'false')
+    imagem.addEventListener('dragstart', drag)
     return imagem;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("imgid", ev.target.id);
+}
+
+function drop(ev) {
+    const imgid= ev.dataTransfer.getData("imgid");
+    const imagem = document.querySelector(`#${imgid}`)
+    imagem.parentElement.addEventListener('dragover', allowDrop)
+    ev.target.appendChild(imagem);
+    ev.target.removeEventListener('dragover', allowDrop)
+    jogadorDaVez()
 }
