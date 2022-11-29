2cf45e6a125ea13dbb7dcc18c1dc4d54b3aa408e
Guilherme Bleidão <guilhermenbleidao@gmail.com>
2022-11-29 04:07:26 -0300

    exibindo e recebendo login

--- autenticacao/index.js
+++ autenticacao/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path');
 
 const porta = 3000;
 const app = express()
 
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get('/', function(req, res) {
-  res.json({teste: 'teste'})
+  res.sendFile(path.join(__dirname + '/public/login.html'))
 })
 
-app.listen(port, function() {
+app.post('/', function(req, res) {
+  console.log(req.body.login, req.body.senha)
+})
+
+app.listen(porta, function() {
   console.log("Escutando na porta", porta)
 })
