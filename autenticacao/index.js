efa2dbae78c1df9aa5bc55ae63dbb8c8558d020b
Guilherme Bleidão <guilhermenbleidao@gmail.com>
2022-11-29 03:43:30 -0300

    express

--- autenticacao/index.js
+++ autenticacao/index.js
@@ -1,14 +1,12 @@
-const http = require('http');
+const express = require('express')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const porta = 3000;
+const app = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', function(req, res) {
+  res.json({teste: 'teste'})
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, function() {
+  console.log("Escutando na porta", porta)
+})
